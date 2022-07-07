c32db2d30706b6e9215593137a404974c4b85324
william Ruiz <william.a23@gmail.com>
2022-07-07 14:26:37 -0500

    ciclo for

---
+++ For/src/For.java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main(String[] args) throws Exception {
+        for (int numeroFor = 0; numeroFor <=3; numeroFor+=1) {
+            System.out.println(numeroFor);
+
+        }
+        
+    }
+}
